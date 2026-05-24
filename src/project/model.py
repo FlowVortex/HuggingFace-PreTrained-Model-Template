1673cc748ddd0caa6b994f2518d73515a47a677f
wwhenxuan <wwhenxuan@gmail.com>
2026-05-24 17:56:05 +0800

    whenxuan: update the model architecture

--- src/project/model.py
+++ src/project/model.py
@@ -1,12 +1,13 @@
 """
 This file is primarily used to implement the specific structure and logic of the model.
 The model needs to be passed a specific Huggingface configuration file to build the model.
-For models with multiple different layer architectures, it is recommended to create an 
+For models with multiple different layer architectures, it is recommended to create an
 additional `layers.py` file to implement the specific layer architecture of the model.
 And only put the core parts of the model, such as the `backbone`, in this file.
 
 We also need to further consider the compatibility of the model itself with the `pipeline` and `trainer`.
 """
+
 import copy
 from dataclasses import dataclass
 from typing import cast, Union, Optional, Tuple
@@ -30,7 +31,7 @@ class ProjectModel(PreTrainedModel):
     def __init__(self, config: ProjectConfig) -> None:
         super().__init__(config)
 
-        # 在这里创建模型的内部module和执行初始化
+        # This is where you create the model's internal modules and perform initialization.
 
         # Initialize weights and apply final processing
         # self.post_init()
@@ -49,19 +50,21 @@ class ProjectModel(PreTrainedModel):
         self,
     ) -> None:
         """
-        这个方法用于检验输入的数据是否符合要求，
-        这个方法并不会对数据进行任何的处理也不会强制性的返回任何东西
-        它只会用于检验输入的数据是否符合要求
-        当输入的数据不符合要求时，这个方法会抛出异常
+        This method is used to check if the input data meets the requirements.
+
+        This method does not perform any data processing and does not forcibly return anything.
+        It only checks if the input data meets the requirements.
+
+        When the input data does not meet the requirements, this method will throw an exception.
         """
 
     def _compute_loss(
         self, *args, **kwargs
     ) -> Union[torch.FloatTensor, torch.cuda.FloatTensor, float]:
         """
-        这个方法用于计算模型的损失
-        这个方法会返回一个浮点数，这个浮点数是模型的损失
-        这个损失会随着模型的output被一起返回
+        This method is used to calculate the model's loss and returns a floating-point number.
+
+        To simplify the downstream model training process, we usually calculate the model loss during the forward propagation of the pre-trained model.
         """
 
     def forward(self, *args, **kwargs) -> ProjectOutput:
