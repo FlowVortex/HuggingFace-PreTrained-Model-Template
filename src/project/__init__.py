16d10c7e1228945bee0a74e537ccb6d0fb88bfbf
wwhenxuan <wwhenxuan@gmail.com>
2026-05-24 18:16:09 +0800

    whenxuan: update the init

---
+++ src/project/__init__.py
@@ -0,0 +1,13 @@
+# Set the version of the project
+__version__ = "0.0.1"
+
+# Create a list of modules that can be directly imported
+# We will put all functions or classes that can be directly imported in __init__.py here.
+__all__ = []
+
+
+# Here we typically import the following modules: config, specific models, pipeline, and trainer for training models.
+# from .config import ProjectConfig, ProjectTrainingConfig
+# from .model import ProjectModel
+# from .pipeline import ...
+# from .trainer import ...
